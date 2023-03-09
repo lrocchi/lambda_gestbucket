50b59c39f38a0f34463e6946d447051f919ba5bd
lrocchi <lrocchi73@gmail.com>
2023-03-09 18:03:24 +0100

    aggiunto invi oa coda sqs

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 "use strict";
-
-const http = require(process.env.protocol);
+import { SendMessageCommand } from "@aws-sdk/client-sqs";
+const http = require(process.env.PnSsGestoreRepositoryProtocol);
 const AWS = require("aws-sdk");
 const crypto = require("crypto");
 
-
+const sqs = new AWS.SQS();
 const s3 = new AWS.S3();
-const HOSTNAME = process.env.hostname;
-const PORT = process.env.port;
-const PATHGET  = process.env.path_getDocument;
-const PATHPATCH = process.env.path_patchDocument;
-const STAGINGBUCKET = process.env.staging_bucket;
+
+const HOSTNAME = process.env.PnSsHostname;
+const PORT = process.env.PnSsGestoreRepositoryPort;
+const PATHGET = process.env.PnSsGestoreRepositoryPathGetDocument;
+const PATHPATCH = process.env.PnSsGestoreRepositoryPathPatchDocument;
+const STAGINGBUCKET = process.env.PnSsStagingBucketName;
 
 let jsonDocument = {
   documentKey: "",
   documentState: "",
 };
 exports.handler = async (event) => {
-  console.log(event);
+  console.log(JSON.stringify(event));
   let bucketName;
   console.log("Buket Name: " + event.Records[0].s3.bucket.name);
   console.log("Object Key: " + event.Records[0].s3.object.key);
@@ -32,20 +33,29 @@ exports.handler = async (event) => {
     case "ObjectCreated:*":
       break;
     case "ObjectCreated:Put":
-      if(bucketName === STAGINGBUCKET){
+      if (bucketName === STAGINGBUCKET) {
         jsonDocument.documentState = "staged";
-      }else{
+      } else {
         jsonDocument.contentLenght = event.Records[0].s3.object.size;
         jsonDocument.documentState = "available";
         params.Bucket = bucketName;
         params.Key = jsonDocument.documentKey;
         const { Body } = await s3.getObject(params).promise();
         console.log(Body);
-        let doc = await getDocumentFromDB(jsonDocument.documentKey);
+        const doc = await getDocumentFromDB(jsonDocument.documentKey);
         console.log(doc);
-        jsonDocument.checkSum = crypto.createHash(doc.documentType.checksum).update(Body).digest("hex"); 
+        console.log(doc.document);
+        console.log(doc.document.documentType.checksum);
+        console.log(JSON.stringify(doc.document.documentType.checksum));
+        jsonDocument.checkSum = crypto
+          .createHash(doc.document.documentType.checksum)
+          .update(Body)
+          .digest("hex");
       }
       console.log(jsonDocument);
+      break;
+    case "ObjectCreated:Copy":
+      jsonDocument.documentState = "available";
       break;
     case "ObjectRestore:Completed":
       jsonDocument.documentState = "available";
@@ -66,15 +76,11 @@ exports.handler = async (event) => {
       const response = {
         statusCode: 200,
       };
-    return response;
+      return response;
   }
-  const res = await updateDynamo(jsonDocument);
+  const res = await updateDynamo(jsonDocument, event);
   console.log(res);
   console.log("############## EXIT  ####################");
-  const response = {
-    statusCode: 200,
-  };
-  return response;
 };
 function getDocumentFromDB(docKey) {
   const options = {
@@ -84,11 +90,10 @@ function getDocumentFromDB(docKey) {
     path: PATHGET + "/" + docKey,
     headers: {
       "Content-Type": "application/json",
-    }, 
+    },
   };
   return new Promise((resolve, reject) => {
     const req = http.request(options, (res) => {
-      
       let responseBody = "";
 
       res.on("data", (chunk) => {
@@ -107,31 +112,73 @@ function getDocumentFromDB(docKey) {
     req.end();
   });
 }
-function updateDynamo(data) {
+function updateDynamo(data, eventData, retries = 3, backoff = 2000) {
   const options = {
     method: "PATCH",
     hostname: HOSTNAME,
     port: PORT,
-    path: PATHPATCH + "/" + docKey,
+    path: PATHPATCH + "/" + data.documentKey,
     headers: {
       "Content-Type": "application/json",
-    }, 
+    },
   };
   return new Promise((resolve, reject) => {
-    const req = http.requesst(options, (res) => {
+    const req = http.request(options, (res) => {
       let responseBody = "";
       res.on("data", (chunk) => {
         responseBody += chunk;
       });
       res.on("end", () => {
         resolve(JSON.parse(responseBody));
+        switch (res.statusCode) {
+          case 404:
+            reject;
+            break;
+          case 500:
+            if (retries > 0) {
+              setTimeout(() => {
+                req.destroy();
+                return updateDynamo(data, eventData, retries - 1, backoff * 2);
+              }, backoff);
+            } else {
+              console.log("FINITO =>" + responseBody);
+              sendOnQueue(eventData);
+            }
+            break;
+          default:
+            resolve(JSON.parse(responseBody));
+            break;
+        }
       });
     });
     req.on("error", (err) => {
       console.error(err);
       reject(err);
     });
-    req.write(data);
+    req.write(JSON.stringify(data));
     req.end();
+  });
+}
+
+function sendOnQueue(msg) {
+  const params = {
+    DelaySeconds: 10,
+    MessageAttributes: {
+      Author: {
+        DataType: "String",
+        StringValue: "Preeti",
+      },
+    },
+    MessageBody: msg,
+    QueueUrl:
+      "https://sqs.us-east-1.amazonaws.com/672607396920/lambda-sqs-demo-queue",
+  };
+
+  sqs.sendMessage(params, (err, data) => {
+    if (err) {
+      console.err("Error", err);
+    } else {
+      console.log("Success", data.MessageId);
+    }
   });
 }
